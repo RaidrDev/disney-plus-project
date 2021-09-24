51cfc8520c3ed1d98898b46b02ff4840b7e88ff3
RaidrDev <raidrdev@gmail.com>
2021-09-25 01:05:55 +0200

    FINISH THE WEB

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,105 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { auth, provider } from '../firebase';
 import styled from 'styled-components'
+import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import {
+    selectUserName,
+    selectUserPhoto,
+    setUserLogin, 
+    setSignOut
+} from '../features/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Header() {
+
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const userName = useSelector(selectUserName);
+    const userPhoto = useSelector(selectUserPhoto);
+
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user)=>{
+            if(user){
+                dispatch(setUserLogin({
+                    name: user.displayName,
+                    email: user.email,
+                    photo: user.photoURL
+                }))
+                history.push("/");
+            }
+        })
+    }, [])
+
+    const signIn = () => {
+        auth.signInWithPopup(provider).then((result)=>{
+            let user = result.user;
+            dispatch(setUserLogin({
+                name: user.displayName,
+                email: user.email,
+                photo: user.photoURL
+            }))
+        })
+    }
+
+    const signOut = () => {
+        auth.signOut().then(()=>{
+            dispatch(setSignOut());
+            history.push("/login");
+        })
+    }
+
     return (
         <Nav>
-            <Link to={``}>  
+            <Link to={``}>
                 <Logo src="/images/logo.svg" />
             </ Link>
-            <NavMenu>
-                <Link to={``}>
-                <a>
-                    <img src="/images/home-icon.svg" />
-                    <span>HOME</span>
-                </a>
-                </ Link>
+            {!userName ? (
+                <LoginContainer>
+                    <Login onClick={signIn}>Login</Login>
+                </LoginContainer>
+                ) :
+                <>
+                    <NavMenu>
+                        <Link to={``}>
+                            <a>
+                                <img src="/images/home-icon.svg" />
+                                <span>HOME</span>
+                            </a>
+                        </ Link>
 
-                <a>
-                    <img src="/images/search-icon.svg" />
-                    <span>SEARCH</span>
-                </a>
+                        <a>
+                            <img src="/images/search-icon.svg" />
+                            <span>SEARCH</span>
+                        </a>
 
-                <a>
-                    <img src="/images/watchlist-icon.svg" />
-                    <span>WATCHLIST</span>
-                </a>
+                        <a>
+                            <img src="/images/watchlist-icon.svg" />
+                            <span>WATCHLIST</span>
+                        </a>
 
-                <a>
-                    <img src="/images/original-icon.svg" />
-                    <span>ORIGINALS</span>
-                </a>
+                        <a>
+                            <img src="/images/original-icon.svg" />
+                            <span>ORIGINALS</span>
+                        </a>
 
-                <a>
-                    <img src="/images/movie-icon.svg" />
-                    <span>MOVIES</span>
-                </a>
+                        <a>
+                            <img src="/images/movie-icon.svg" />
+                            <span>MOVIES</span>
+                        </a>
 
-                <a>
-                    <img src="/images/series-icon.svg" />
-                    <span>SERIES</span>
-                </a>
+                        <a>
+                            <img src="/images/series-icon.svg" />
+                            <span>SERIES</span>
+                        </a>
 
-            </NavMenu>
+                    </NavMenu>
 
-            <UserImg src= "https://www.alucoildesign.com/assets/pages/media/profile/profile_user.jpg" />
+                    <UserImg onClick={signOut}
+                     src="https://www.alucoildesign.com/assets/pages/media/profile/profile_user.jpg" />
+                </>
+            }
+
 
         </Nav>
     )
@@ -125,6 +181,28 @@ const UserImg = styled.img`
     height: 48px;
     border-radius: 50%;
     cursor: pointer;
+`
 
+const Login = styled.div`
+    border-radius: 4px;
+    border: 1px solid #f9f9f9;
+    padding: 8px 16px;
+    letter-spacing: 1.5px;
+    text-transform: uppercase;
+    background-color: rgba(0, 0, 0, 0.6);
+    transition: all 250ms ease 0s;
+    cursor: pointer;
 
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
+    }
+
+`
+
+const LoginContainer = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
 `
